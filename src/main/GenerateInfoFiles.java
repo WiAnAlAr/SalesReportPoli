48cc2b481ae6dd2e4d3dff0c8861fd21eb640981
William Alejo <alejowilliam@test.com>
2024-09-06 20:42:37 -0500

    se agregan las listas de documentos, tipos de documentos, nombres y apellidos. se modifica la creacion del documento para agregar multiples registros

--- src/main/GenerateInfoFiles.java
+++ src/main/GenerateInfoFiles.java
@@ -4,16 +4,24 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class GenerateInfoFiles {
-	static List<String> options = Arrays.asList("Cedula", "Pasaporte", "Cedula Extranjeria");
+	static List<String> docTypes = Arrays.asList("Cedula", "Pasaporte", "Cedula Extranjeria");
+	static List<String> names = Arrays.asList("Pepe", "Pedro", "Juan", "Luis", "Carlos", "José", "Miguel", "Andrés", "Diego", "Antonio", "Manuel", "Javier", "Fernando", "Ricardo", "Alejandro", "Hugo", "Martín", "Daniel", "Santiago", "Pablo");
+	static List<String> lastNames = Arrays.asList("García", "Martínez", "Rodríguez", "López", "Hernández", "Pérez", "Sánchez", "Ramírez", "Torres", "Flores", "González", "Fernández", "Castro", "Vargas", "Morales", "Reyes", "Mendoza", "Romero", "Díaz", "Cruz");
+	static List<String> products = Arrays.asList("Televisor", "Teléfono móvil", "Laptop", "Cámara fotográfica", "Auriculares", "Reloj inteligente", "Tablet", "Teclado mecánico", "Consola de videojuegos", "Impresora"
+);
+	static List<String> documents = new ArrayList<>();
 	static Random random = new Random();
 	public GenerateInfoFiles() {
 		deletTxtFiles();
-		createSalesBySellerFile(1);			
+		for(int i = 0; i < random.nextInt(10) + 1; i++) {
+			createSalesBySellerFile(i+1);
+		}					
 	}
 	
 	private void createFile(String fileName, List<String> content) {
@@ -29,6 +37,7 @@ public class GenerateInfoFiles {
         }
         System.out.println(fileName + ".txt" + " has been created.");
     }
+	
 	private void deletTxtFiles() {
         String projectDirectoryPath = System.getProperty("user.dir");
         File directory = new File(projectDirectoryPath);
@@ -47,20 +56,30 @@ public class GenerateInfoFiles {
             System.out.println(projectDirectoryPath + " is not a directory or does not exist.");
         }        
     }
+	
 	public String createSalerDocTypeDocNum() {                
-        String document = options.get(random.nextInt(options.size()));
+        String document = docTypes.get(random.nextInt(docTypes.size()));
+        
         int randomNumber = random.nextInt(1000000000) + 1000000000;
-        return document + ";" + randomNumber;
+        String documentInfo =  document + ";" + randomNumber;
+        documents.add(documentInfo);
+        return documentInfo;
     }
+	
 	public String createSaleRecord() {		
         int randomProductId = random.nextInt(10) + 1;
         int randomQuantitySold = random.nextInt(5) + 1;
         return randomProductId + ";" + randomQuantitySold;
 	}
+	
 	public void createSalesBySellerFile(int Amount){
 		String seller = createSalerDocTypeDocNum();
-		String saleInfo = createSaleRecord();
-		List<String> records = Arrays.asList(seller, saleInfo);
+		List<String> records =  new ArrayList<>();
+		records.add(seller);
+		for(int i = 0; i < random.nextInt(10) + 1;i++) {
+			String saleInfo = createSaleRecord();
+			records.add(saleInfo);
+		}				
 		createFile("Saler" + Amount, records);
 	}
 }
